a88a8c4c135989d5f7b4f3cfc6b39a0d0a0ba4b3
Krzysztof Kwieciński <k.d.kwiecinski@gmail.com>
2023-12-30 21:40:04 +0100

    chore: initialize Spring Boot project with SQL dependencies

---
+++ backend/src/main/java/kk/tech/playground/TechPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package kk.tech.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechPlaygroundApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kk/tech/playground/TechPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package kk.tech.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
